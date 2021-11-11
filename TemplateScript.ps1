0080638b05a0022d3a2a84d075f2a89bafdb060d
Aetopia <41850963+Aetopia@users.noreply.github.com>
2021-11-11 22:49:41 +0530

    Add files via upload

---
+++ TemplateScript.ps1
@@ -0,0 +1,13 @@
+﻿$option = $args[0]
+Write-Output "Sample Installer Script"
+Write-Output $option
+# Install
+if ($option -eq "-i") {
+    Write-Output "This is the installation part of the script."
+}
+elseif ($option -eq "-u") {
+    Write-Output "This is the updation part of the script."
+}
+elseif ($option -eq "-r") {
+    Write-Output "This is the removal/uninstallation part of the script."
+}
