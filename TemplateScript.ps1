ab0b8c50cb8958e417517caa2d58e0f2974759a8
Aetopia <41850963+Aetopia@users.noreply.github.com>
2021-11-11 22:57:58 +0530

    Update TemplateScript.ps1

--- TemplateScript.ps1
+++ TemplateScript.ps1
@@ -1,13 +1,13 @@
-﻿$option = $args[0]
+$option = $args[0]
 Write-Output "Sample Installer Script"
 Write-Output $option
 # Install
-if ($option -eq "-i") {
+if ($option -eq "install") {
     Write-Output "This is the installation part of the script."
 }
-elseif ($option -eq "-u") {
+elseif ($option -eq "update") {
     Write-Output "This is the updation part of the script."
 }
-elseif ($option -eq "-r") {
+elseif ($option -eq "remove") {
     Write-Output "This is the removal/uninstallation part of the script."
 }
